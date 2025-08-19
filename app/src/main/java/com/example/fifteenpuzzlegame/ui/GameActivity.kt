e4d4c7956e1708032100689752fb2a3ecdc59078
SabrinaRashidova <rashidovasabrina2@gmail.com>
2025-08-18 23:24:40 -0400

    Game over dialog added

--- app/src/main/java/com/example/fifteenpuzzlegame/ui/GameActivity.kt
+++ app/src/main/java/com/example/fifteenpuzzlegame/ui/GameActivity.kt
@@ -2,13 +2,19 @@ package com.example.fifteenpuzzlegame.ui
 
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.GridLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fifteenpuzzlegame.R
 import com.example.fifteenpuzzlegame.R.color
 import com.example.fifteenpuzzlegame.databinding.ActivityGameBinding
+import com.example.fifteenpuzzlegame.databinding.DialogGameOverBinding
 
 class GameActivity : AppCompatActivity() {
      lateinit var binding: ActivityGameBinding
@@ -23,7 +29,8 @@ class GameActivity : AppCompatActivity() {
 
         gridLayout = binding.gridLayout
         initList()
-        shuffleNumber()
+
+//        shuffleNumber()
         updateGridUI()
         binding.txtRestart.setOnClickListener {
             restartGame()
@@ -98,9 +105,47 @@ class GameActivity : AppCompatActivity() {
                 if (isAdjacent(clickedIndex, emptyTileIndex)) {
                     swapTiles(clickedIndex, emptyTileIndex)
                     emptyTileIndex = clickedIndex
+
+                    if (checkWin()){
+                        val dialogbinding: DialogGameOverBinding = DialogGameOverBinding.inflate(layoutInflater)
+                        val builder = AlertDialog.Builder(this)
+                            .setView(dialogbinding.root)
+
+                        val dialog = builder.create()
+                        dialog.setContentView(dialogbinding.root)
+                        dialog.show()
+                        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                        dialog.window?.attributes?.windowAnimations= R.style.DialogAnimation
+                        dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+                        dialog.window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+                        dialog.window?.setDimAmount(0.6f)
+
+                        dialogbinding.btnExit.setOnClickListener { finish() }
+                        dialogbinding.btnPlayAgain.setOnClickListener {
+                            dialog.dismiss()
+                            Handler(mainLooper).postDelayed({
+                                restartGame()
+                            },2000)
+                        }
+
+                    }
                 }
             }
     }}
+
+    fun checkWin(): Boolean{
+        for (i in 0 until 15){
+            val value= tiles[i].text.toString()
+            if (value != (i+1).toString()){
+                return false
+            }
+        }
+        return tiles[15].text.toString().isEmpty()
+    }
+
+//    fun isSolvable(numbers: List<Int>): Boolean {
+//
+//    }
 
     override fun onPause() {
         super.onPause()
